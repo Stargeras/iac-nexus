000a7fd438cda94d183be74272540e0a61cbdca2
Stargeras <dillongrosso@live.com>
2022-02-17 07:52:01 -0500

    fix vars

--- main.tf
+++ main.tf
@@ -4,4 +4,6 @@ module "nexus" {
     service_type = "${var.service_type}"
     ingress_hostname = "${var.ingress_hostname}"
     storage_class = "${var.storage_class}"
+    storage_size = "${var.storage_size}"
+    ingress_secret = "${var.ingress_secret}"
 }
