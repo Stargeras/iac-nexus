17dae3ad940f31e1f3bb2f1d5a36e5662eaef1bb
Stargeras <dillongrosso@live.com>
2022-02-17 20:56:12 -0500

    fixes

--- main.tf
+++ main.tf
@@ -3,7 +3,6 @@ module "nexus" {
     kubeconfig = "${var.kubeconfig}"
     service_type = "${var.service_type}"
     ingress_hostname = "${var.ingress_hostname}"
-    ingress_secret = "${var.ingress_secret}"
     storage_class = "${var.storage_class}"
     storage_size = "${var.storage_size}"
 }
--- module-docker-repos/main.tf
+++ module-docker-repos/main.tf
@@ -16,14 +16,14 @@ resource "nexus_repository_docker_hosted" "repositories" {
     for_each = toset(var.docker_repositories)
     name = "${each.key}"
     online = true
-    http_port = "${each.value}"
     docker {
-      force_basic_auth = false
-      v1_enabled       = false
+        http_port = "${each.value}"
+        force_basic_auth = false
+        v1_enabled       = false
     }
     storage {
-      blob_store_name                = "default"
-      strict_content_type_validation = true
-      write_policy                   = "ALLOW"
+        blob_store_name                = "default"
+        strict_content_type_validation = true
+        write_policy                   = "ALLOW"
     }
 }
