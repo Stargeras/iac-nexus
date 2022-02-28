eb995585a28b3c084e307af88c13d968870a75e0
Stargeras <dillongrosso@live.com>
2022-02-28 14:16:44 -0500

    add maven repos

--- module-nexus-config/repositories.tf
+++ module-nexus-config/repositories.tf
@@ -13,3 +13,37 @@ resource "nexus_repository_docker_hosted" "repositories" {
         write_policy                   = "ALLOW"
     }
 }
+
+resource "nexus_repository" "maven-releases" {
+    for_each = var.tenants
+    name = "${each.key}-private-maven-2-relases"
+    format = "maven2"
+    type = "hosted"
+
+    storage {
+        blob_store_name                = "default"
+        strict_content_type_validation = true
+        write_policy                   = "ALLOW"
+    }
+
+    maven {
+        version_policy = "RELEASE"
+    }
+}
+
+resource "nexus_repository" "maven-snapshots" {
+    for_each = var.tenants
+    name = "${each.key}-private-maven-2-snapshots"
+    format = "maven2"
+    type = "hosted"
+
+    storage {
+        blob_store_name                = "default"
+        strict_content_type_validation = true
+        write_policy                   = "ALLOW"
+    }
+
+    maven {
+        version_policy = "RELEASE"
+    }
+}
